9d81bdc41560bf383ab5c3a3dedfb57be061cb9f
Aaron Hon-Yu Ma <hyma128@pu.edu.tw>
2025-09-25 11:46:44 +0800

    use cursor to execute sql instruction

--- sqlctrl.py
+++ sqlctrl.py
@@ -5,13 +5,19 @@ conn = sqlite3.connect('product_info.db')
 cursor = conn.cursor()
 
 # using permission to execute sql instruction
-cursor.execute('SELECT * FROM product_info')
+sqlsel="""
+SELECT id, name FROM product_info
+WHERE id='2'
+"""
+cursor.execute(sqlsel)
 
 # grab execution result and save to python variable, print variable
 records = cursor.fetchall()
 print (records)
 
+
 # execute sqlctrl.py
 #1: in terminal: python sqlctrl.py
 #2: in terminal: python3 sqlctrl.py
 #3: in VSCode: click 'Run Python File'
+
